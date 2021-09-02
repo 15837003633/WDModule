deda113807c5a0ee07822aaf9df07d2455492001
wudang <50165459@qq.com>
2021-09-02 09:27:43 +0800

    2222

---
+++ Classes/People.h
@@ -0,0 +1,17 @@
+//
+//  People.h
+//  WdTest
+//
+//  Created by mac on 2021/8/2.
+//
+
+#import <Foundation/Foundation.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface People : NSObject
+@property (nonatomic, strong) NSString *name;
+
+@end
+
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/People.m
@@ -0,0 +1,21 @@
+//
+//  People.m
+//  WdTest111
+//
+//  Created by mac on 2021/8/2.
+//
+
+#import "People.h"
+
+@implementation People
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+//        self.age = @"11";
+    }
+    return self;
+}
+@end
